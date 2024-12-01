855941b169038b7cc8867b16cfa1083f0f388977
Javier <jgcarrasco@student.agh.edu.pl>
2024-12-01 21:49:34 +0100

    Design and save plot

--- funs.py
+++ funs.py
@@ -1,4 +1,3 @@
-from tkinter import filedialog, messagebox
 import tkinter as tk
 from tkinter import ttk
 import os
@@ -8,60 +7,73 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
 
 
-def load_file(root, title):
-    file = filedialog.askopenfilename(
+def load_file(root, header):
+    file = tk.filedialog.askopenfilename(
         title="Select file",
         filetypes=[("CSV", "*.csv")]
     )
     if file:
         load_file.df = pd.read_csv(file)
-        title.grid_forget()
-        file_name = os.path.basename(file)
         if all(hasattr(load_file, attr) for attr in ["name_file", "path_file"]):  
             load_file.name_file.grid_forget() 
             load_file.path_file.grid_forget()
 
-        load_file.name_file = tk.Label(root, text=f"Name of the file: {file_name}", font=("Times New Roman", 13))
-        load_file.name_file.grid(row=1, column=2)
-        load_file.path_file = tk.Label(root, text=f"Path of the file: {os.path.dirname(file)}", font=("Times New Roman", 13))
-        load_file.path_file.grid(row=2, column=2)
-        data_content = tk.Text(root)
-        data_content.grid(row=3, column=2)
+        file_info = tk.Frame(root)
+        file_info.grid(row=1, column=0, columnspan=1, sticky="nsew", pady=20)
 
-        scrollbar = tk.Scrollbar(root, command=data_content.yview)
-        scrollbar.grid(row=3, column=3, sticky="nsw")
+        tk.Label(file_info, text="File information:", font=("Times New Roman", 15, "bold")).grid(row=0, column=0, sticky="w", padx=20)
+
+        load_file.info_label = tk.Label(
+            file_info, 
+            text=f"- Name: {os.path.basename(file)}\n- Path: {os.path.dirname(file)}\n- Size: {os.path.getsize(file)/ (1024 * 1024):.2f} MB", 
+            font=("Times New Roman", 13),
+            justify="left"
+        )
+        load_file.info_label.grid(row=1, column=0, sticky="w", padx=20)
+
+        data_frame = tk.Frame(root)
+        data_frame.grid(row=2, column=0, sticky="nsew", padx=20)
+        tk.Label(data_frame, text="Data:", font=("Times New Roman", 15, "bold")).grid(row=0, column=0, sticky="w")
+        data_content = tk.Text(data_frame)
+        data_content.grid(row=1, column=0, sticky="nsew", pady=8)
+        scrollbar = tk.Scrollbar(data_frame, command=data_content.yview)
+        scrollbar.grid(row=1, column=1, sticky="nsw")
+
         data_content.config(yscrollcommand=scrollbar.set)
         try: 
             with open(file, "r") as f:
                 reader = csv.reader(f)  
                 rows = [", ".join(row) for row in reader]
-                columns = list(load_file.df.columns) #rows[0].split(",")
+                columns = list(load_file.df.columns) 
                 content = "\n".join(rows)
                 data_content.insert("1.0", content)
             data_content.config(state="disabled")
 
         except Exception as e:
-            messagebox.showerror("Error", f"Error reading file: {e}")
+            tk.messagebox.showerror("Error", f"Error reading file: {e}")
 
-        x_axis = ttk.Combobox(root, state="readonly", values=columns)
-        x_axis.grid(row=5, column=1, sticky="w")
-        x_axis_label = tk.Label(root, text="X-axis:")
-        x_axis_label.grid(row=5, column=0, sticky="e")
-        y_axis = ttk.Combobox(root, state="readonly", values=columns)
-        y_axis.grid(row=5, column=4, sticky="w")
-        y_axis_label = tk.Label(root, text="Y-axis:")
-        y_axis_label.grid(row=5, column=3, sticky="e")
+        axis_frame = tk.Frame(root)
+        axis_frame.grid(row=3, column=0, sticky="nsew", padx=20, pady=12)
 
-        plot = tk.Button(root, text="Plot", font=("Times New Roman", 13), command=lambda: plot_data(root, x_axis.get(), y_axis.get()))
-        plot.grid(row=4, column=2)
+        x_axis_label = tk.Label(axis_frame, text="X-axis:", font=("Times New Roman", 15, "bold"))
+        x_axis_label.grid(row=0, column=0, sticky="e")
+        x_axis = ttk.Combobox(axis_frame, state="readonly", values=columns)
+        x_axis.grid(row=0, column=1, sticky="w")
+        y_axis_label = tk.Label(axis_frame, text=f"{' '*46}Y-axis:", font=("Times New Roman", 15, "bold"))
+        y_axis_label.grid(row=0, column=2, sticky="e")
+        y_axis = ttk.Combobox(axis_frame, state="readonly", values=columns)
+        y_axis.grid(row=0, column=3, sticky="w")
+
+        plot = tk.Button(header, text="Plot", font=("Courier", 13), command=lambda: plot_data(data_frame, header, x_axis.get(), y_axis.get()), width=20, bg="#F7F7F7")
+        plot.grid(row=0, column=3, padx=5, pady=10)
 
     else:
-        messagebox.showwarning("Warning", "No file selected")
+        tk.messagebox.showwarning("Warning", "No file selected")
 
 
-def plot_data(root, x_axis, y_axis):
+def plot_data(data_frame, header, x_axis, y_axis):
     if not x_axis or not y_axis:
-        messagebox.showwarning("Warning", "Please select both X and Y axes")
+        tk.messagebox.showwarning("Warning", "Please select both X and Y axes")
         return
 
     try:
@@ -69,6 +81,7 @@ def plot_data(root, x_axis, y_axis):
         x_data = df[x_axis]
         y_data = df[y_axis]
 
+        tk.Label(data_frame, text="Plot:", font=("Times New Roman", 15, "bold")).grid(row=0, column=2, sticky="w", padx=20)
         fig = Figure(figsize=(5, 4), dpi=100)
         ax = fig.add_subplot(111)
         ax.plot(x_data, y_data, "o", label=f"{y_axis} vs. {x_axis}")
@@ -76,9 +89,26 @@ def plot_data(root, x_axis, y_axis):
         ax.set_ylabel(y_axis)
         ax.legend()
 
-        canvas = FigureCanvasTkAgg(fig, master=root)
+        canvas = FigureCanvasTkAgg(fig, master=data_frame)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=7, column=0, columnspan=6)
+        canvas.get_tk_widget().grid(row=1, column=2, columnspan=3, padx=20, pady=8)
+
+        plot = tk.Button(header, text="Save plot", font=("Courier", 13), command=lambda: save_plot(fig), width=20, bg="#F7F7F7")
+        plot.grid(row=0, column=4, padx=5, pady=10)
 
     except Exception as e:
-        messagebox.showerror("Error", f"Error generating plot: {e}")
+        tk.messagebox.showerror("Error", f"Error generating plot: {e}")
+
+
+def save_plot(fig):
+    try:
+        file_path = tk.filedialog.asksaveasfilename(
+            defaultextension=".png", 
+            filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg")])
+
+        if file_path:  
+            fig.savefig(file_path)
+            tk.messagebox.showinfo("Success", "Plot saved successfully!")
+
+    except Exception as e:
+        tk.messagebox.showerror("Error", f"Error saving plot: {e}")
